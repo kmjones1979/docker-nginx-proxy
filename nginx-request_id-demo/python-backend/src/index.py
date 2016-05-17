5d7bc8b969741da301daba4205e089bf332b2237
Kevin Jones <jones.kevin@gmail.com>
2016-05-17 14:38:24 -0400

    adding request header check

--- nginx-request_id-demo/python-backend/src/index.py
+++ nginx-request_id-demo/python-backend/src/index.py
@@ -4,8 +4,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    reqi = request.headers.get('x-request-id')
-    return 'X-Request-ID: ' + reqi
+    if 'x-request-id' in request.headers:
+        reqi = request.headers.get('x-request-id')
+        return 'X-Request-ID: ' + reqi
+        for x in request.headers:
+            return x
+    else:
+        return 'Please pass a X-Request-ID header'
 
 if __name__ == '__main__':
     app.run()
