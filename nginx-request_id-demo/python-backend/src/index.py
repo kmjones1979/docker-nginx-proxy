06e6bfe7727713e237ab9737eea93e5cea4d50b2
Kevin Jones <jones.kevin@gmail.com>
2016-05-17 01:13:28 -0400

    refactor using flsk instead of web.py

--- nginx-request_id-demo/python-backend/src/index.py
+++ nginx-request_id-demo/python-backend/src/index.py
@@ -1,24 +1,11 @@
-import web
-import logging
+from flask import Flask
+from flask import request
+app = Flask(__name__)
 
-# log setup
-logging.basicConfig(filename='/var/log/python/python.log',level=logging.DEBUG)
+@app.route('/')
+def index():
+    reqi = request.headers.get('x-request-id')
+    return 'X-Request-ID: ' + reqi
 
-urls = (
-  '/', 'index'
-)
-
-class index:
-    def GET(self):
-        xreq = web.ctx.env['HTTP_X_REQUEST_ID']
-        if xreq == "":
-            return 'Please pass a header named X-Request-ID'
-            logging.info(xreq)
-        else:
-            return 'X-Request-ID: ' + xreq + '\n'
-            return web.ctx.env
-            logging.info(xreq)
-
-if __name__ == "__main__":
-    app = web.application(urls, globals())
+if __name__ == '__main__':
     app.run()
