7883ebbec727e1f5cae38be85a37e67484d7ae3a
Kevin Jones <jones.kevin@gmail.com>
2016-05-18 18:40:50 -0400

    adding changes to go backend

--- nginx-request_id-demo/go-backend/src/hello-world/main.go
+++ nginx-request_id-demo/go-backend/src/hello-world/main.go
@@ -1,15 +1,39 @@
 package main
 
 import (
-	"io"
+	"encoding/json"
+	"fmt"
 	"net/http"
 )
 
-func hello(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "Hello world!")
+type Profile struct {
+	Name    string
+	Hobbies []string
 }
 
 func main() {
-	http.HandleFunc("/", hello)
+	http.HandleFunc("/", foo)
+	http.HandleFunc("/bar", bar)
 	http.ListenAndServe(":7001", nil)
+}
+
+func foo(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Server", "A Go Web Server")
+	w.WriteHeader(200)
+	for key, value := range r.Header {
+		fmt.Println(key+": ", value)
+	}
+}
+
+func bar(w http.ResponseWriter, r *http.Request) {
+	profile := Profile{"Kevin", []string{"art", "nerd"}}
+
+	js, err := json.Marshal(profile)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
 }
