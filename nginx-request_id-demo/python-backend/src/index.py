06d44c603a7cde66ffedb0a87d2397fdc963d361
Kevin Jones <jones.kevin@gmail.com>
2016-05-18 12:40:51 -0400

    logging support for flask requests

--- nginx-request_id-demo/python-backend/src/index.py
+++ nginx-request_id-demo/python-backend/src/index.py
@@ -1,13 +1,18 @@
+import logging
+from logging.handlers import RotatingFileHandler
 from flask import Flask
 from flask import request
+
 app = Flask(__name__)
+
+log = '/var/log/python/python.log'
 
 @app.route('/')
 def index():
     print request.environ
     kv = request.headers
     for k, v in kv.iteritems():
-        print (request.method + ' ' + request.environ.get('PATH_INFO') +
+        app.logger.info(request.method + ' ' + request.environ.get('PATH_INFO') +
                 request.environ.get('QUERY_STRING') + ' ' +
                 request.environ.get('SERVER_PROTOCOL') + " " +
                 " ".join(map(lambda (k, v): '%s: "%s"' % (k, v), kv.iteritems())))
@@ -26,4 +31,10 @@ for k, v in kv.iteritems():
 """
 
 if __name__ == '__main__':
+    formatter = logging.Formatter("[%(asctime)s] {%(pathname)s:%(lineno)d} %(levelname)s - %(message)s")
+    handler = RotatingFileHandler(log, maxBytes=10000, backupCount=1)
+    handler.setLevel(logging.DEBUG)
+    handler.setFormatter(formatter)
+    app.logger.addHandler(handler)
+    app.logger.setLevel(logging.DEBUG)
     app.run(host='0.0.0.0')
