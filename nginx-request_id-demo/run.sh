ca7460683768af7d5780ed9ae7deb907cb9c6c62
Kevin Jones <jones.kevin@gmail.com>
2016-05-19 13:52:52 -0400

    adding changes of ports and adding support for go to image to use supervisord

--- nginx-request_id-demo/run.sh
+++ nginx-request_id-demo/run.sh
@@ -1,4 +1,4 @@
-docker run -i -t -p 5000:5000 --name python-backend -d python-backend
 docker run -i -t -p 3001:3001 --name node-backend -d node-backend
-docker run -i -t -p 7001:7001 --name go-backend -d go-backend
-docker run -i -t -p 80:80 -p 6001:6001 --name nginx-frontend -d nginx-frontend
+docker run -i -t -p 5001:5001 --name python-backend -d python-backend
+docker run -i -t -p 6001:6001 --name go-backend -d go-backend
+docker run -i -t -p 80:80 -p 2001:2001 --name nginx-frontend -d nginx-frontend
