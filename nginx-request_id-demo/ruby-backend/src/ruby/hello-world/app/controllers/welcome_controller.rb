3c88b035ddadb6987827f32cb30cd70e1858450f
Kevin Jones <jones.kevin@gmail.com>
2016-05-23 14:25:21 -0400

    adding logger support for real now

--- nginx-request_id-demo/ruby-backend/src/ruby/hello-world/app/controllers/welcome_controller.rb
+++ nginx-request_id-demo/ruby-backend/src/ruby/hello-world/app/controllers/welcome_controller.rb
@@ -1,5 +1,6 @@
 require 'json'
 require 'sse'
+require 'time'
 
 class WelcomeController < ApplicationController
 
@@ -11,12 +12,16 @@ class WelcomeController < ApplicationController
     response.headers['Content-Type'] = 'text/event-stream'
     sse = ServerSide::SSE.new(response.stream)
     begin
+      http_logger.info "HTTP REQUEST START"
+      http_logger << Time.now.iso8601
       request.env.each do |header|
 	if header[0] =~ /^(REQUEST_|PATH_|QUERY_|REMOTE_|SERVER_|SCRIPT_|ORIGINAL_|HTTP_)(.*)/
           sse.write("#{header[0]}: " "#{header[1]}")
-	  http_logger.info("#{header[0]}: " "#{header[1]}").join("\n")
+	  http_logger << " #{header[0]}: " "#{header[1]}"
 	end
       end
+      http_logger << "\n"
+      http_logger.info "HTTP REQUEST END"
     rescue IOError
     ensure
       sse.close
