a100300dc6e6ed9bd327204cf7e95d184f070b25
Kevin Jones <jones.kevin@gmail.com>
2016-05-23 14:10:35 -0400

    adding support for logging

--- nginx-request_id-demo/ruby-backend/src/ruby/hello-world/app/controllers/welcome_controller.rb
+++ nginx-request_id-demo/ruby-backend/src/ruby/hello-world/app/controllers/welcome_controller.rb
@@ -3,18 +3,25 @@ require 'sse'
 
 class WelcomeController < ApplicationController
 
+  def http_logger
+    @@http_logger ||= Logger.new("/var/log/ruby/ruby.log")
+  end
+
   def index
     response.headers['Content-Type'] = 'text/event-stream'
     sse = ServerSide::SSE.new(response.stream)
     begin
       request.env.each do |header|
-        sse.write("#{header[0]}: " "#{header[1]}")
-        # create log line here with join
+	if header[0] =~ /^(REQUEST_|PATH_|QUERY_|REMOTE_|SERVER_|SCRIPT_|ORIGINAL_|HTTP_)(.*)/
+          sse.write("#{header[0]}: " "#{header[1]}")
+	  http_logger.info("#{header[0]}: " "#{header[1]}").join("\n")
+	end
       end
     rescue IOError
     ensure
       sse.close
     end
+
   end
 
 end
