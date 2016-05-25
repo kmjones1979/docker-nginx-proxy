d28522f9dfc4d66b4f7945c0acf59d4b1ebe028b
Kevin Jones <jones.kevin@gmail.com>
2016-05-25 14:01:28 -0400

    changes to move amplify agent to nginx-frontent and remove from go-backend

--- nginx-request_id-demo/build.sh
+++ nginx-request_id-demo/build.sh
@@ -1,4 +1,9 @@
-docker build -t nginx-frontend nginx-frontend/.
+#!/bin/bash
+
+echo -n "Enter your Amplify API key > "
+read api_key
+
+docker build --build-arg API_KEY="$api_key" -t nginx-frontend nginx-frontend/.
 docker build -t python-backend python-backend/.
 docker build -t node-backend node-backend/.
 docker build -t go-backend go-backend/.
